71c2441ce1dd2c721aeacf66b47aac395d072bdf
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-06-04 19:56:43 +0100

    feat: get results from tally contract

--- plugins/maciVoting/components/PollCard.tsx
+++ plugins/maciVoting/components/PollCard.tsx
@@ -4,18 +4,22 @@ import { useMaci } from "../hooks/useMaci";
 import { VoteOption } from "../utils/types";
 import { PleaseWaitSpinner } from "@/components/please-wait";
 import { PUBLIC_MACI_ADDRESS } from "@/constants";
-import { getPoll } from "@maci-protocol/sdk/browser";
+import { getPoll, getResults, type IResult } from "@maci-protocol/sdk/browser";
 import { useEthersSigner } from "../hooks/useEthersSigner";
 import { unixTimestampToDate } from "../utils/formatPollDate";
+import { useCoordinator } from "../hooks/useCoordinator";
 
 const PollCard = ({ pollId }: { pollId: bigint }) => {
   // check if the user joined the poll
   const { setPollId, onJoinPoll, onVote, isRegistered, hasJoinedPoll, isLoading, error: maciError } = useMaci();
+  const { checkIsTallied } = useCoordinator();
   const signer = useEthersSigner();
   const [error, setError] = useState<string | undefined>(undefined);
   const [voteStartDate, setVoteStartDate] = useState(0);
   const [voteEnded, setVoteEnded] = useState(false);
   const [voteOption, setVoteOption] = useState<VoteOption | undefined>(undefined);
+  const [results, setResults] = useState<IResult[] | undefined>(undefined);
+  const [tallied, setTallied] = useState(false);
 
   const disabled = useMemo(() => {
     return isLoading || voteEnded || voteStartDate > Math.round(Date.now() / 1000);
@@ -43,8 +47,30 @@ const PollCard = ({ pollId }: { pollId: bigint }) => {
       setVoteEnded(endDate < now);
     };
 
+    const checkPollTallied = async () => {
+      const isTallied = await checkIsTallied(Number(pollId));
+      setTallied(isTallied);
+    };
+
+    const getPollResults = async () => {
+      const contractResults = await getResults({
+        maciAddress: PUBLIC_MACI_ADDRESS,
+        pollId: pollId.toString(),
+        signer,
+      });
+      setResults(contractResults);
+    };
+
     checkVoteEnded();
-  }, [voteEnded, setVoteEnded, pollId, signer]);
+
+    if (voteEnded) {
+      checkPollTallied();
+    }
+
+    if (tallied) {
+      getPollResults();
+    }
+  }, [voteEnded, setVoteEnded, pollId, signer, checkIsTallied, tallied, getResults]);
 
   useEffect(() => {
     setPollId(pollId);
@@ -83,16 +109,42 @@ const PollCard = ({ pollId }: { pollId: bigint }) => {
     return "Join poll";
   }, [hasJoinedPoll, isLoading]);
 
-  if (voteEnded)
+  if (voteEnded && !tallied)
     return (
       <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
         <Heading size="h3">MACI Poll</Heading>
         <div className="flex flex-col justify-between">
           <p className="text-sm text-critical-500">{error}</p>
         </div>
-        <p>The voting period has ended. You can no longer vote.</p>
+        <p>The voting period has ended and you can no longer vote. Please wait for the results to be tallied.</p>
       </Card>
     );
+
+  if (voteEnded && tallied && !results)
+    return (
+      <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
+        <Heading size="h3">MACI Poll</Heading>
+        <div className="flex flex-col justify-between">
+          <p className="text-sm text-critical-500">{error}</p>
+        </div>
+        <p>The results have been tallied. Results will be displayed here soon.</p>
+      </Card>
+    );
+
+  if (voteEnded && tallied && results) {
+    return (
+      <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
+        <Heading size="h3">Results</Heading>
+        <div className="flex flex-col justify-between">
+          <p className="text-sm text-critical-500">{error}</p>
+        </div>
+        <p>The voting period has ended. Here are the results:</p>
+        <p style={{ color: "green" }}>Yes: {results[0].value.toString()}</p>
+        <p style={{ color: "red" }}>No: {results[1].value.toString()}</p>
+        <p style={{ color: "blue" }}>Abstain: {results[2].value.toString()}</p>
+      </Card>
+    );
+  }
 
   if (!isRegistered)
     return (
